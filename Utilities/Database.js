6d7dbb3cd0ebd807f7c4fe2e33566012e4874f3b
mikemills254 <mikemills930@gmail.com>
2024-02-06 12:30:57 +0300

    updating the authcontroller

---
+++ Utilities/Database.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+class Database {
+    static async connect() {
+        try {
+            if (!process.env.DB_URI) {
+                throw new Error("DB_URI environment variable is not defined.");
+            }
+
+            await mongoose.connect(process.env.DB_URI, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            });
+
+            return mongoose.connection;
+        } catch (error) {
+            throw new Error("Error connecting to MongoDB: " + error.message);
+        }
+    }
+
+    static async disconnect() {
+        try {
+            await mongoose.disconnect();
+            return "Disconnected from MongoDB using Mongoose";
+        } catch (error) {
+            throw new Error("Error disconnecting from MongoDB: " + error.message);
+        }
+    }
+}
+
+export default Database;
